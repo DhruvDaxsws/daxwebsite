4d3b0842dafb5cea6c3abe72ec3df72c0023109c
dhruv daxsws <dhruvdaxsws@gmail.com>
2025-11-29 13:44:00 +0000

    image is not visible

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-white">
-      <div className="container mx-auto flex h-[108px] items-center justify-between px-4 lg:px-6">
+      <div className="container mx-auto flex h-[108px] items-center justify-between px-4 md:px-6">
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center space-x-2">
             <DaxLogo />
